6b63100ddae0dd3e8c8f336c8ac762a964807220
Anushka-gaikwad <anushkagaikwad0111@gmail.com>
2026-02-05 18:30:38 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test" {
   ami                    = "ami-0f5ee92e2d63afc18"
-  instance_type          = t2.medium
+  instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name = aws_key_pair.ec2_key.key_name
