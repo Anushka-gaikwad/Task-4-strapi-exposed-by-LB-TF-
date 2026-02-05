c7606e685aa8aec2ec71db8402603a973455c724
Anushka-gaikwad <anushkagaikwad0111@gmail.com>
2026-02-05 12:48:09 +0530

    Create main.tf

---
+++ module/ec2/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "test" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = t2.medium
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_name
+  user_data              = file("${path.root}/user_data.sh")
+}
