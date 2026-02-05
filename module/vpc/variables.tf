65fe281a41d11578d284bd69435d00d12a24e9bf
Anushka-gaikwad <anushkagaikwad0111@gmail.com>
2026-02-05 16:28:09 +0530

    Update variables.tf

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,15 +1,20 @@
-variable "env" {
-  default = ap-south-1
+variable "region" {
+  type    = string
+  default = "ap-south-1"
 }
 
 variable "vpc_cidr" {
-  default = 10.0.0.0/16
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  default = 10.0.0.7/24
+  type    = string
+  default = "10.0.0.0/24"
 }
 
 variable "private_subnet_cidr" {
-  default = 10.0.0.8/24
+  type    = string
+  default = "10.0.1.0/24"
 }
+
